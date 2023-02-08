7c9e932df445f2c2a6cb0a42bc8f4b741775e3fd
ass55 <aadit.shah2@sky.uk>
2023-02-08 16:34:08 +0000

    Trial3.2 - Fixing Generation of DoxyDoc Trial2.1

--- scripts/doxygen_errors/doxygen_errors.sh
+++ scripts/doxygen_errors/doxygen_errors.sh
@@ -10,12 +10,12 @@ results=$?
 if [ $results -eq 0 ];
    then
 	# If doxygen does not have any warnings, code exits with zero status
-	echo "Doxygen documentation succesfully generated!"
+	echo "DOXYGEN DOCUMENTATION SUCCESSFULLY GENERATED!"
 	exit 0
 elif [ $results -ne 0 ];
    then
 	# If doxygen generated any warnings, code exits with non - zero status
-	echo "Doxygen generated warnings!"
+	echo "DOXYGEN GENERATED WARNINGS!"
 	exit 1
 fi
 
