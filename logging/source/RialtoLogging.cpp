1ceff3a7b8e3a1fc99daf9120ec204a638bfc41f
aadit.shah <aadit.shah2@sky.uk>
2023-03-07 13:25:28 +0000

    RIALTO_COMPONENT_EXTERNAL and default return values changed

--- logging/source/RialtoLogging.cpp
+++ logging/source/RialtoLogging.cpp
@@ -73,6 +73,7 @@ static std::string componentToString(RIALTO_COMPONENT component)
         return "COM";
         break;
     case RIALTO_COMPONENT_EXTERNAL:
+        return "EXT:" break;
     default:
         return "UNK";
         break;
