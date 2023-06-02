8a7c68996d6d4f3bcd01038b2e85ff626cf52f8c
aadit.shah <aadit.shah2@sky.uk>
2023-06-02 10:23:34 +0100

    comments resolved

--- media/public/include/IMediaPipeline.h
+++ media/public/include/IMediaPipeline.h
@@ -1158,7 +1158,7 @@ public:
      * @brief Set level and transition of audio attenuation.
      *        Sets the current volume for the pipeline (0.0 silent -> 1.0 full volume)
      *
-     * @param[in] volume Target volume level (0.0 - 1.0)
+     * @param[in] volume : Target volume level (0.0 - 1.0)
      *
      * @retval true on success false otherwise
      */
@@ -1167,7 +1167,7 @@ public:
     /**
      * @brief Get current audio level. Fetches the current volume level for the pipeline.
      *
-     * @param[out] volume Current volume level (range 0.0 - 1.0)
+     * @param[out] volume : Current volume level (range 0.0 - 1.0)
      *
      * @retval true on success false otherwise
      */
@@ -1180,7 +1180,7 @@ public:
      * unmuted the user will hear audio at the same volume as previously
      * set.
      *
-     * @param[in] mute   Desired mute state, true=muted, false=not muted
+     * @param[in] mute : Desired mute state, true=muted, false=not muted
      *
      * @retval true on success false otherwise
      */
@@ -1189,7 +1189,7 @@ public:
     /**
      * @brief Get current mute status of the pipeline
      *
-     * @param[out] mute   Current mute state
+     * @param[out] mute : Current mute state
      *
      * @retval true on success false otherwise
      */
