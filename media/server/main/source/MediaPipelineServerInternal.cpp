b2bfb88c20d8eb0de49b20cdc79d4f2170cbccd7
aadit.shah <aadit.shah2@sky.uk>
2023-08-18 13:25:31 +0100

    threshold increase to 200 - commit id

--- media/server/main/source/MediaPipelineServerInternal.cpp
+++ media/server/main/source/MediaPipelineServerInternal.cpp
@@ -29,7 +29,7 @@
 
 namespace
 {
-constexpr unsigned int kMaxSkippedNoAvailableSamples = 160;
+constexpr unsigned int kMaxSkippedNoAvailableSamples = 200;
 constexpr std::chrono::milliseconds kNeedMediaDataResendTimeMs{100};
 const char *toString(const firebolt::rialto::MediaSourceStatus &status)
 {
