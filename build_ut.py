726b2e430c6102696a70c015d11ba9860d040996
luke <luke.williamson@sky.uk>
2023-09-12 13:25:19 +0100

    Add Wrapper exclude to client and clientipc

--- build_ut.py
+++ build_ut.py
@@ -241,7 +241,7 @@ def AddValgrind(suite, outputToFile, outputToXml):
 
 def generateCoverageReport(outputDir, resultsFile, suites):
     # The lcov command will fail if the --exclude file does not exist, only run '--exclude *Wrapper*' for the relevent suites
-    ExcludeWrapperSuitesBase = ['serveripc', 'serverservice', 'servergstplayer', 'servermain', 'manager']
+    ExcludeWrapperSuitesBase = ['serveripc', 'serverservice', 'servergstplayer', 'servermain', 'manager', 'clientipc', 'client']
     ExcludeWrapperSuitesTest = ['servergstplayer', 'servermain', 'manager']
 
     lcovBaseCmd = ["lcov", "-c", "-i", "-d", ".", "--output-file", "coverage_base.info", "--exclude", "/usr/*",
