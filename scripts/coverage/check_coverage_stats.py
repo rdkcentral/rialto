b0faec0a0466c4fe45d123f5f68aaa5d2710c131
luke <luke.williamson@sky.uk>
2023-11-23 09:43:50 +0000

    Fix check_coverage_stats script

--- scripts/coverage/check_coverage_stats.py
+++ scripts/coverage/check_coverage_stats.py
@@ -23,12 +23,11 @@ import sys
 
 def main():
     if len(sys.argv) < 3:
-        write_output("Can't check coverage stats - Wrong number of script arguments")
+        print("Can't check coverage stats - Wrong number of script arguments")
         sys.exit("Wrong number of script arguments")
     stats = parse_statistics(sys.argv[1])
     percentage_failure = sys.argv[2]
-    comparison_output = check_coverage(stats, percentage_failure)
-    write_output(comparison_output)
+    check_coverage(stats, percentage_failure)
 
 def parse_statistics(file_path):
     try:
@@ -41,24 +40,22 @@ def parse_statistics(file_path):
         file.close()
         return (float(covered_lines_str), float(covered_functions_str))
     except:
-        write_output("Can't compare coverage stats - Could not open statistics file")
+        print("Can't compare coverage stats - Could not open statistics file")
         return (0.0, 0.0)
 
 def check_coverage(stats, percentage_failure):
     is_failure = False
 
-    if stats[0] < percentage_failure:
-        print("Line coverage " + str(stats[0]) + " is less than minimum failure percentage " + str(percentage_failure))
+    if stats[0] < float(percentage_failure):
+        print("Line coverage " + str(stats[0]) + " is less than minimum failure percentage " + percentage_failure)
         is_failure = True
 
-    if stats[1] < percentage_failure:
-        print("Function coverage " + str(stats[1]) + " is less than minimum failure percentage " + str(percentage_failure))
+    if stats[1] < float(percentage_failure):
+        print("Function coverage " + str(stats[1]) + " is less than minimum failure percentage " + percentage_failure)
         is_failure = True
 
     if is_failure:
         sys.exit("Coverage stats failed, Line coverage: " + str(stats[0]) + ", Function coverage: " + str(stats[1]))
-    
-    return output_text
 
 if __name__ == "__main__":
     main()
