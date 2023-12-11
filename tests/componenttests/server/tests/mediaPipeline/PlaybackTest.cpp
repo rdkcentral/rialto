de71b6f625a9bf069e3b6a53afde9926808bcad3
Marcin Wojciechowski <marcin.wojciechowski@sky.uk>
2023-12-11 12:33:30 +0100

    playback test comment

--- tests/componenttests/server/tests/mediaPipeline/PlaybackTest.cpp
+++ tests/componenttests/server/tests/mediaPipeline/PlaybackTest.cpp
@@ -27,13 +27,137 @@ constexpr int kFrameCountInPausedState{3};
 constexpr int kFrameCountInPlayingState{24};
 } // namespace
 
+/*
+ * Component Test: Audio Video Playback Sequence
+ * Test Objective:
+ *  Test the playback of video and audio MSE content. The test transitions through the playback states
+ *  buffering 1 frame of both audio and video content before preroll and 1 frame of both audio and video
+ *  content after preroll. The session is then terminated. All the metadata and media data written to the
+ *  shared buffer is checked for accuracy.
+ *
+ * Sequence Diagrams:
+ *  Create, Destroy - https://wiki.rdkcentral.com/display/ASP/Rialto+MSE+Misc+Sequence+Diagrams
+ *  Start/Resume Playback, Pause Playback, Stop, End of stream
+ *   - https://wiki.rdkcentral.com/display/ASP/Rialto+Playback+Design
+ *
+ * Test Setup:
+ *  Language: C++
+ *  Testing Framework: Google Test
+ *  Components: MediaPipeline
+ *
+ * Test Initialize:
+ *  Set Rialto Server to Active
+ *  Connect Rialto Client Stub
+ *  Map Shared Memory
+ *
+ * Test Steps:
+ *  Step 1: Create a new media session
+ *   Send CreateSessionRequest to Rialto Server
+ *   Expect that successful CreateSessionResponse is received
+ *   Save returned session id
+ *
+ *  Step 2: Load content
+ *   Send LoadRequest to Rialto Server
+ *   Expect that successful LoadResponse is received
+ *   Expect that GstPlayer instance is created.
+ *   Expect that client is notified that the NetworkState has changed to BUFFERING.
+ *
+ *  Step 3: Attach all sources
+ *   Attach the audio source.
+ *   Expect that audio source is attached.
+ *   Attach the video source.
+ *   Expect that video source is attached.
+ *   Expect that rialto source is setup
+ *   Expect that all sources are attached.
+ *   Expect that the Playback state has changed to IDLE.
+ *
+ *  Step 4: Pause
+ *   Pause the content.
+ *   Expect that gstreamer pipeline is paused.
+ *   Expect that server notifies the client that the Network state has changed to PAUSED.
+ *
+ *  Step 5: Write 1 audio frame
+ *   Gstreamer Stub notifies, that it needs audio data
+ *   Expect that server notifies the client that it needs 3 frames of audio data.
+ *   Write 1 frame of audio data to the shared buffer.
+ *   Send HaveData message
+ *   Expect that server notifies the client that it needs 3 frames of audio data.
+ *
+ *  Step 6: Write 1 video frame
+ *   Gstreamer Stub notifies, that it needs video data
+ *   Expect that server notifies the client that it needs 3 frames of video data.
+ *   Write 1 frame of video data to the shared buffer.
+ *   Send HaveData message
+ *   Expect that server notifies the client that it needs 3 frames of video data.
+ *
+ *  Step 7: Notify buffered
+ *   Expect that server notifies the client that the Network state has changed to BUFFERED.
+ *
+ *  Step 8: Play
+ *   Play the content.
+ *   Expect that gstreamer pipeline is in playing state
+ *   Expect that server notifies the client that the Playback state has changed to PLAYING.
+ *
+ *  Step 9: Write 1 audio frame
+ *   Write 1 frame of audio data to the shared buffer.
+ *   Send HaveData
+ *   Expect that server notifies the client that it needs 24 frames of audio data.
+ *
+ *  Step 10: Write 1 video frame
+ *   Write 1 frame of video data to the shared buffer.
+ *   Send HaveData
+ *   Expect that server notifies the client that it needs 24 frames of video data.
+ *
+ *  Step 11: End of audio stream
+ *   Send audio haveData with one frame and EOS status
+ *   Expect that Gstreamer is notified about end of stream
+ *
+ *  Step 12: End of video stream
+ *   Send video haveData with one frame and EOS status
+ *   Expect that Gstreamer is notified about end of stream
+ *
+ *  Step 13: Notify end of stream
+ *   Simulate, that gst_message_eos is received by Rialto Server
+ *   Expect that server notifies the client that the Network state has changed to END_OF_STREAM.
+ *
+ *  Step 14: Remove sources
+ *   Remove the audio source.
+ *   Expect that audio source is removed.
+ *   Remove the video source.
+ *   Expect that video source is removed.
+ *
+ *  Step 15: Stop
+ *   Stop the playback.
+ *   Expect that stop propagated to the gstreamer pipeline.
+ *   Expect that server notifies the client that the Playback state has changed to STOPPED.
+ *
+ *  Step 16: Destroy media session
+ *   Send DestroySessionRequest.
+ *   Expect that the session is destroyed on the server.
+ *
+ * Test Teardown:
+ *  Memory region created for the shared buffer is unmapped.
+ *  Server is terminated.
+ *
+ * Expected Results:
+ *  All API calls are handled by the server.
+ *  The state of the Gstreamer Pipeline is successfully negotiationed in the normal playback scenario.
+ *  Data is successfully read from the shared memory and pushed to gstreamer pipeline for both audio and video.
+ *
+ * Code:
+ */
 namespace firebolt::rialto::server::ct
 {
 TEST_F(MediaPipelineTest, playback)
 {
+    // Step 1: Create a new media session
     createSession();
+
+    // Step 2: Load content
     gstPlayerWillBeCreated();
     load();
+
+    // Step 3: Attach all sources
     audioSourceWillBeAttached();
     attachAudioSource();
     videoSourceWillBeAttached();
@@ -44,8 +168,14 @@ TEST_F(MediaPipelineTest, playback)
     willSetupAndAddSource(&m_videoAppSrc);
     willFinishSetupAndAddSource();
     indicateAllSourcesAttached();
+
+    // Step 4: Pause
     willPause();
     pause();
+
+    // Step 5: Write 1 audio frame
+    // Step 6: Write 1 video frame
+    // Step 7: Notify buffered
     gstNeedData(&m_audioAppSrc, kFrameCountInPausedState);
     gstNeedData(&m_videoAppSrc, kFrameCountInPausedState);
     {
@@ -59,20 +189,36 @@ TEST_F(MediaPipelineTest, playback)
         EXPECT_EQ(receivedNetworkStateChange->session_id(), m_sessionId);
         EXPECT_EQ(receivedNetworkStateChange->state(), ::firebolt::rialto::NetworkStateChangeEvent_NetworkState_BUFFERED);
     }
+
+    // Step 8: Play
     willPlay();
     play();
+
+    // Step 9: Write 1 audio frame
+    // Step 10: Write 1 video frame
     pushAudioData(kFramesToPush, kFrameCountInPlayingState);
     pushVideoData(kFramesToPush, kFrameCountInPlayingState);
+
+    // Step 11: End of audio stream
+    // Step 12: End of video stream
     willEos(&m_audioAppSrc);
     eosAudio(kFramesToPush);
     willEos(&m_videoAppSrc);
     eosVideo(kFramesToPush);
+
+    // Step 13: Notify end of stream
     gstNotifyEos();
     willRemoveAudioSource();
+
+    // Step 14: Remove sources
     removeSource(m_audioSourceId);
     removeSource(m_videoSourceId);
+
+    // Step 15: Stop
     willStop();
     stop();
+
+    // Step 16: Destroy media session
     gstPlayerWillBeDestructed();
     destroySession();
 }
