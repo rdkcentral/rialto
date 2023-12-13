54f3ca234cd60034b1827d017ec966ecdfce756f
Marcin Wojciechowski <marcin.wojciechowski@sky.uk>
2023-12-13 10:33:31 +0100

    set position failure

--- tests/componenttests/server/tests/mediaPipeline/SetPositionTest.cpp
+++ tests/componenttests/server/tests/mediaPipeline/SetPositionTest.cpp
@@ -97,6 +97,40 @@ public:
         EXPECT_EQ(receivedVideoNeedData->source_id(), m_videoSourceId);
         m_lastVideoNeedData = receivedVideoNeedData;
     }
+
+    void willFailToSetPosition()
+    {
+        EXPECT_CALL(*m_gstWrapperMock, gstElementSeek(&m_pipeline, kPlaybackRate, GST_FORMAT_TIME,
+                                                      static_cast<GstSeekFlags>(GST_SEEK_FLAG_FLUSH), GST_SEEK_TYPE_SET,
+                                                      kPositionInPaused, GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE))
+            .WillOnce(Return(FALSE));
+    }
+
+    void SetPositionFailure()
+    {
+        // During SetPosition procedure, two PlaybackStateChange events are received. Add expects for them.
+        ExpectMessage<PlaybackStateChangeEvent> expectedSeekingPlaybackStateChange{m_clientStub};
+        ExpectMessage<PlaybackStateChangeEvent> expectedFailurePlaybackStateChange{m_clientStub};
+        expectedSeekingPlaybackStateChange.setFilter(
+            [&](const PlaybackStateChangeEvent &event)
+            { return event.state() == PlaybackStateChangeEvent_PlaybackState_SEEKING; });
+        expectedFailurePlaybackStateChange.setFilter(
+            [&](const PlaybackStateChangeEvent &event)
+            { return event.state() == PlaybackStateChangeEvent_PlaybackState_FAILURE; });
+
+        // Send SetPositionRequest and expect success
+        auto request{createSetPositionRequest(m_sessionId, kPositionInPaused)};
+        ConfigureAction<SetPosition>(m_clientStub).send(request).expectSuccess();
+
+        // Check received PlaybackStateChange events
+        auto receivedSeekingPlaybackStateChange{expectedSeekingPlaybackStateChange.getMessage()};
+        ASSERT_TRUE(receivedSeekingPlaybackStateChange);
+        EXPECT_EQ(receivedSeekingPlaybackStateChange->session_id(), m_sessionId);
+
+        auto receivedFlushedPlaybackStateChange{expectedFailurePlaybackStateChange.getMessage()};
+        ASSERT_TRUE(receivedFlushedPlaybackStateChange);
+        EXPECT_EQ(receivedFlushedPlaybackStateChange->session_id(), m_sessionId);
+    }
 };
 
 /*
@@ -276,9 +310,9 @@ TEST_F(SetPositionTest, SetPosition)
 
     // Step 13: Notify end of stream
     gstNotifyEos();
-    willRemoveAudioSource();
 
     // Step 14: Remove sources
+    willRemoveAudioSource();
     removeSource(m_audioSourceId);
     removeSource(m_videoSourceId);
 
@@ -287,6 +321,157 @@ TEST_F(SetPositionTest, SetPosition)
     stop();
 
     // Step 16: Destroy media session
+    gstPlayerWillBeDestructed();
+    destroySession();
+}
+
+/*
+ * Component Test: Set position failure states
+ * Test Objective:
+ *  Test that seek failure is successfully handled.
+ *
+ * Sequence Diagrams:
+ *  Seek - https://wiki.rdkcentral.com/display/ASP/Rialto+Seek+Design
+ *
+ * Test Setup:
+ *  Language: C++
+ *  Testing Framework: Google Test
+ *  Components: MediaPipeline
+ *
+ * Test Initialize:
+ *  Set Rialto Server to Active
+ *  Connect Rialto Client Stub
+ *  Map Shared Memory
+ *
+ * Test Steps:
+ *  Step 1: Create a new media session
+ *   Send CreateSessionRequest to Rialto Server
+ *   Expect that successful CreateSessionResponse is received
+ *   Save returned session id
+ *
+ *  Step 2: Load content
+ *   Send LoadRequest to Rialto Server
+ *   Expect that successful LoadResponse is received
+ *   Expect that GstPlayer instance is created.
+ *   Expect that client is notified that the NetworkState has changed to BUFFERING.
+ *
+ *  Step 3: Attach all sources
+ *   Attach the audio source.
+ *   Expect that audio source is attached.
+ *   Attach the video source.
+ *   Expect that video source is attached.
+ *   Expect that rialto source is setup
+ *   Expect that all sources are attached.
+ *   Expect that the Playback state has changed to IDLE.
+ *
+ *  Step 4: Pause
+ *   Pause the content.
+ *   Expect that gstreamer pipeline is paused.
+ *   Expect that server notifies the client that the Network state has changed to PAUSED.
+ *
+ *  Step 5: Write 1 audio frame
+ *   Gstreamer Stub notifies, that it needs audio data
+ *   Expect that server notifies the client that it needs 3 frames of audio data.
+ *   Write 1 frame of audio data to the shared buffer.
+ *   Send HaveData message
+ *   Expect that server notifies the client that it needs 3 frames of audio data.
+ *
+ *  Step 6: Write 1 video frame
+ *   Gstreamer Stub notifies, that it needs video data
+ *   Expect that server notifies the client that it needs 3 frames of video data.
+ *   Write 1 frame of video data to the shared buffer.
+ *   Send HaveData message
+ *   Expect that server notifies the client that it needs 3 frames of video data.
+ *
+ *  Step 7: Notify buffered
+ *   Expect that server notifies the client that the Network state has changed to BUFFERED.
+ *
+ *  Step 8: SetPosition Failure
+ *   SetPosition to position 10s.
+ *   Server should notify the client that the Playback state has changed to SEEKING.
+ *   Expect that SetPositionResponse has success status
+ *   Server should notify the client that the Playback state has changed to FAILED.
+ *
+ *  Step 14: Remove sources
+ *   Remove the audio source.
+ *   Expect that audio source is removed.
+ *   Remove the video source.
+ *   Expect that video source is removed.
+ *
+ *  Step 15: Stop
+ *   Stop the playback.
+ *   Expect that stop propagated to the gstreamer pipeline.
+ *   Expect that server notifies the client that the Playback state has changed to STOPPED.
+ *
+ *  Step 16: Destroy media session
+ *   Send DestroySessionRequest.
+ *   Expect that the session is destroyed on the server.
+ *
+ * Test Teardown:
+ *  Memory region created for the shared buffer is unmapped.
+ *  Server is terminated.
+ *
+ * Expected Results:
+ *  Set position is handled and forwarded to the gstreamer.
+ *
+ * Code:
+ */
+TEST_F(SetPositionTest, SetPositionFailure)
+{
+    // Step 1: Create a new media session
+    createSession();
+
+    // Step 2: Load content
+    gstPlayerWillBeCreated();
+    load();
+
+    // Step 3: Attach all sources
+    audioSourceWillBeAttached();
+    attachAudioSource();
+    videoSourceWillBeAttached();
+    attachVideoSource();
+    sourceWillBeSetup();
+    setupSource();
+    willSetupAndAddSource(&m_audioAppSrc);
+    willSetupAndAddSource(&m_videoAppSrc);
+    willFinishSetupAndAddSource();
+    indicateAllSourcesAttached();
+
+    // Step 4: Pause
+    willPause();
+    pause();
+
+    // Step 5: Write 1 audio frame
+    // Step 6: Write 1 video frame
+    // Step 7: Notify buffered
+    gstNeedData(&m_audioAppSrc, kFrameCount);
+    gstNeedData(&m_videoAppSrc, kFrameCount);
+    {
+        ExpectMessage<firebolt::rialto::NetworkStateChangeEvent> expectedNetworkStateChange{m_clientStub};
+
+        pushAudioData(kFramesToPush, kFrameCount);
+        pushVideoData(kFramesToPush, kFrameCount);
+
+        auto receivedNetworkStateChange{expectedNetworkStateChange.getMessage()};
+        ASSERT_TRUE(receivedNetworkStateChange);
+        EXPECT_EQ(receivedNetworkStateChange->session_id(), m_sessionId);
+        EXPECT_EQ(receivedNetworkStateChange->state(), ::firebolt::rialto::NetworkStateChangeEvent_NetworkState_BUFFERED);
+    }
+
+    // Step 8: SetPosition Failure
+    willFailToSetPosition();
+    SetPositionFailure();
+
+    // Step 9: Remove sources
+    willRemoveAudioSource();
+    removeSource(m_audioSourceId);
+    removeSource(m_videoSourceId);
+
+    // Step 10: Stop
+    willStop();
+    stop();
+
+    // Step 11: Destroy media session
     gstPlayerWillBeDestructed();
     destroySession();
 }
