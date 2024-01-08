a45aa38ff0d5664ed1732abbbacd1209286c3f4f
aadit.shah <aadit.shah2@sky.uk>
2024-01-08 16:33:59 +0000

    adding debug to figure out why githubaction bot is not printing out coverage stats

--- scripts/coverage/process_coverage_stats.py
+++ scripts/coverage/process_coverage_stats.py
@@ -235,27 +235,6 @@
 # if __name__ == "__main__":
 #     main()
 
-#!/usr/bin/env python3
-
-#
-# If not stated otherwise in this file or this component's LICENSE file the
-# following copyright and licenses apply:
-#
-# Copyright 2022 Sky UK
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import sys
 
 def main():
@@ -266,11 +245,12 @@ def main():
     current_stats = parse_statistics(sys.argv[2])
     comparison_output = compare_coverage(master_stats, current_stats)
     write_output(comparison_output)
-    
+
     # Check if coverage remains unchanged
     if current_stats == master_stats:
         write_output("Coverage remains unchanged. Exiting.")
-        sys.exit()
+        write_output(comparison_output)
+        sys.exit(1)
 
 def parse_statistics(file_path):
     try:
