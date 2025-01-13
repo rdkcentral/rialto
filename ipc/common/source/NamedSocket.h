756fbe610112038f4268755d4aeb9a0f0aa9fabe
Marcin Wojciechowski <marcin.wojciechowski@sky.uk>
2025-01-13 12:17:07 +0100

    ipc impl

---
+++ ipc/common/source/NamedSocket.h
@@ -0,0 +1,53 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2025 Sky UK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIREBOLT_RIALTO_IPC_NAMED_SOCKET_H_
+#define FIREBOLT_RIALTO_IPC_NAMED_SOCKET_H_
+
+#include "INamedSocket.h"
+
+namespace firebolt::rialto::ipc
+{
+class NamedSocketFactory : public INamedSocketFactory
+{
+public:
+    std::unique_ptr<INamedSocket> createNamedSocket(const std::string &socketPath) const override;
+};
+
+class NamedSocket : public INamedSocket
+{
+public:
+    NamedSocket(const std::string &socketPath);
+    ~NamedSocket() override;
+
+    int getFd() const override;
+
+private:
+    void closeListeningSocket();
+    bool getSocketLock();
+
+private:
+    int m_sockFd{-1};
+    int m_lockFd{-1};
+    std::string m_sockPath{};
+    std::string m_lockPath{};
+};
+} // namespace firebolt::rialto::ipc
+
+#endif // FIREBOLT_RIALTO_IPC_NAMED_SOCKET_H_
