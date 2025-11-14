8903f3fddb687bb9a7779c0700117d38545e1fad
Marcin Wojciechowski <marcin.wojciechowski@sky.uk>
2025-11-14 12:09:15 +0100

    test compilation fix

--- tests/unittests/media/server/mocks/gstplayer/GstGenericPlayerPrivateMock.h
+++ tests/unittests/media/server/mocks/gstplayer/GstGenericPlayerPrivateMock.h
@@ -78,6 +78,7 @@ public:
     MOCK_METHOD(void, startSubtitleClockResyncTimer, (), (override));
     MOCK_METHOD(void, stopSubtitleClockResyncTimer, (), (override));
     MOCK_METHOD(bool, hasSourceType, (const MediaSourceType &mediaSourceType), (const, override));
+    MOCK_METHOD(void, postponeFlush, (const MediaSourceType &mediaSourceType, bool resetTime), (override));
 };
 } // namespace firebolt::rialto::server
 
