4a1104dd3c960c3e9dfc2b4bf71688c616ed082b
Marcin Wojciechowski <marcin.wojciechowski@sky.uk>
2025-12-02 13:38:51 +0100

    ct coverage fix

--- tests/componenttests/client/stubs/MediaPipelineModuleStub.h
+++ tests/componenttests/client/stubs/MediaPipelineModuleStub.h
@@ -42,6 +42,7 @@ public:
     void notifyBufferUnderflowEvent(int sessionId, int32_t sourceId);
     void notifyPlaybackErrorEvent(int sessionId, int32_t sourceId, PlaybackError error);
     void notifySourceFlushed(int sessionId, int32_t sourceId);
+    void notifyPlaybackInfo(int sessionId, const firebolt::rialto::PlaybackInfo &playbackInfo);
 
     // Client helpers
     virtual void waitForClientConnect() = 0;
