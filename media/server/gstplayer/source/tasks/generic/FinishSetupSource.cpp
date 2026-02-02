6d79ad4bebc4d86d3b2857e2320339116efc76e3
Sasa Mudri <Sasa_Mudri@comcast.com>
2026-02-02 15:42:31 +0100

    Updated Profiler and GstProfiler classes.
    Summary: Add profiling to Rialto Server.
    Type: Feature
    Test Plan: UT/CT, Fullstack
    Jira: RDKEMW-117641

--- media/server/gstplayer/source/tasks/generic/FinishSetupSource.cpp
+++ media/server/gstplayer/source/tasks/generic/FinishSetupSource.cpp
@@ -124,6 +124,8 @@ void FinishSetupSource::execute() const
     m_context.setupSourceFinished = true;
 
     RIALTO_SERVER_LOG_MIL("All sources attached.");
-    m_context.m_gstProfiler->createRecord("All Sources Attached");
+    auto recordId = m_context.m_gstProfiler->createRecord("All Sources Attached");
+    if(recordId)
+        m_context.m_gstProfiler->logRecord(recordId.value());
 }
 } // namespace firebolt::rialto::server::tasks::generic
