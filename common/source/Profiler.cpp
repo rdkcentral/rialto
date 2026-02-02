c3a4c76f842b51a8c7ca13dda14347c281400990
Sasa Mudri <Sasa_Mudri@comcast.com>
2026-02-02 15:42:31 +0100

    Updated Profiler and GstProfiler classes.
    Summary: Add profiling to Rialto Server.
    Type: Feature
    Test Plan: UT/CT, Fullstack
    Jira: RDKEMW-117641

--- common/source/Profiler.cpp
+++ common/source/Profiler.cpp
@@ -3,45 +3,113 @@
 
 #include <algorithm>
 
-// namespace firebolt::rialto::common
-// {
-Profiler::Profiler(std::string_view module, uint64_t id)
-    : m_module(module), m_id(id)
+namespace firebolt::rialto::common
+{
+Profiler::Profiler(std::string_view module)
+    : m_module(module)
 {
     std::lock_guard<std::mutex> lock(m_mutex);
     m_records.clear();
 }
 
-void Profiler::record(std::string stage)
+std::optional<Profiler::RecordId> Profiler::record(std::string stage)
 {
     auto now = Clock::now();
     std::lock_guard<std::mutex> lock(m_mutex);
 
-    Record record = Record{m_module, m_id, stage, now};
-    m_records.push_back(record);
+    if (m_records.size() >= kMaxRecords)
+    {
+        return std::nullopt;
+    }
 
+    const Profiler::RecordId id = m_id++;
 
-    const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(record.time.time_since_epoch()).count();
-    RIALTO_COMMON_LOG_FATAL("RECORD: Module[%s] id[%llu] stage=%s time_since_epoch_ns=%lld",
-                            record.module.data(), static_cast<unsigned long long>(record.id), record.stage.c_str(), (long long)ns);
+    m_records.push_back(Record{
+        std::string(m_module),
+        id,
+        std::move(stage),
+        std::string{},
+        now
+    });
+
+    return id;
 }
 
-std::vector<Profiler::Record> Profiler::snapshot() const
+std::optional<Profiler::RecordId> Profiler::record(std::string stage, std::string info)
+{
+    auto now = Clock::now();
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    if (m_records.size() >= kMaxRecords)
+    {
+        return std::nullopt;
+    }
+
+    const Profiler::RecordId id = m_id++;
+
+    m_records.push_back(Record{
+        std::string(m_module),
+        id,
+        std::move(stage),
+        std::move(info),
+        now
+    });
+
+    return id;
+}
+
+std::optional<Profiler::RecordId> Profiler::find(std::string stage)
 {
     std::lock_guard<std::mutex> lock(m_mutex);
-    return m_records;
+
+    for (const auto& record : m_records)
+        if (record.stage == stage)
+            return record.id;
+
+    return std::nullopt;
+}
+
+std::optional<Profiler::RecordId> Profiler::find(std::string stage, std::string info)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    for (const auto& record : m_records)
+        if (record.stage == stage && record.info == info)
+            return record.id;
+
+    return std::nullopt;
+}
+
+void Profiler::log(const RecordId id)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    const auto* record = findById(id);
+    if(record)
+    {
+        const auto us = std::chrono::duration_cast<std::chrono::microseconds>(record->time.time_since_epoch()).count();
+        RIALTO_COMMON_LOG_MIL("PROFILER | RECORD | MODULE[%s] ID[%llu] STAGE[%s] INFO[%s] TIMESTAMP[%lld]",
+                            record->module.data(), static_cast<unsigned long long>(record->id),
+                            record->stage.c_str(), record->info.c_str(), (long long)us);
+    }
 }
 
 void Profiler::dump(std::ostream& os) const
 {
-    auto copy = snapshot();
-    if (copy.empty())
-        return;
-
-    std::sort(copy.begin(), copy.end(),
-            [](const Record& x, const Record& y) { return x.time < y.time; });
-
     // TODO dump()
 }
 
-// }; // namespace firebolt::rialto::common
+const Profiler::Record* Profiler::findById(Profiler::RecordId id)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    for (const auto& record : m_records)
+    {
+        if (record.id == id)
+            return &record;
+    }
+
+    return nullptr;
+}
+
+}; // namespace firebolt::rialto::common
