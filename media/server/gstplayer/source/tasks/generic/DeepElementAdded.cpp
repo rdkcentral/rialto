74f9e5dc000477019153543778aeca77ab9debf0
Sasa Mudri <Sasa_Mudri@comcast.com>
2026-02-05 16:01:42 +0100

    Added descriptions for IProfiler and some small fixes.
    Summary: Add profiling to Rialto Server.
    Type: Feature
    Test Plan: UT/CT, Fullstack
    Jira: RDKEMW-11764

--- media/server/gstplayer/source/tasks/generic/DeepElementAdded.cpp
+++ media/server/gstplayer/source/tasks/generic/DeepElementAdded.cpp
@@ -56,7 +56,7 @@ DeepElementAdded::DeepElementAdded(GenericPlayerContext &context, IGstGenericPla
             }
         }
 
-        m_context.m_gstProfiler->scheduleGstElementRecord(m_element);
+        m_context.gstProfiler->scheduleGstElementRecord(m_element);
     }
 }
 
--- media/server/gstplayer/source/tasks/generic/FinishSetupSource.cpp
+++ media/server/gstplayer/source/tasks/generic/FinishSetupSource.cpp
@@ -124,8 +124,8 @@ void FinishSetupSource::execute() const
     m_context.setupSourceFinished = true;
 
     RIALTO_SERVER_LOG_MIL("All sources attached.");
-    auto recordId = m_context.m_gstProfiler->createRecord("All Sources Attached");
+    auto recordId = m_context.gstProfiler->createRecord("All Sources Attached");
     if(recordId)
-        m_context.m_gstProfiler->logRecord(recordId.value());
+        m_context.gstProfiler->logRecord(recordId.value());
 }
 } // namespace firebolt::rialto::server::tasks::generic
