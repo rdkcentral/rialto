4b1ef447c348a0bedf673f0d2195526743c16622
balasaraswathy-n <Balasaraswathy_N@comcast.com>
2026-02-20 21:20:34 +0530

    Update GenericPlayerTaskFactory.h

--- media/server/gstplayer/include/tasks/generic/GenericPlayerTaskFactory.h
+++ media/server/gstplayer/include/tasks/generic/GenericPlayerTaskFactory.h
@@ -69,7 +69,7 @@ public:
                                                       IGstGenericPlayerPrivate &player) const override;
     std::unique_ptr<IPlayerTask> createCheckAudioUnderflow(GenericPlayerContext &context,
                                                            IGstGenericPlayerPrivate &player) const override;
-    std::unique_ptr<IPlayerTask> createSetPlaybackRate(GenericPlayerContext &context, double rate) const override;
+    std::unique_ptr<IPlayerTask> createSetPlaybackRate(GenericPlayerContext &context, double rate, GstElement *element, GstElement *element) const override;
     std::unique_ptr<IPlayerTask> createSetPosition(GenericPlayerContext &context, IGstGenericPlayerPrivate &player,
                                                    std::int64_t position) const override;
     std::unique_ptr<IPlayerTask> createSetupElement(GenericPlayerContext &context, IGstGenericPlayerPrivate &player,
