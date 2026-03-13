6d912c4fb79cea11aebb028472eee1ea8e12bcd5
Adam Czynszak <adam.czynszak@sky.uk>
2026-03-13 11:22:50 +0000

    Revert "Feature/rdkemw 12692 (#438)"
    This reverts commit 6325af236930c44eccfc96b9b69728b1ff90371b.

--- tests/componenttests/client/mocks/MediaPipelineModuleMock.h
+++ tests/componenttests/client/mocks/MediaPipelineModuleMock.h
@@ -72,13 +72,6 @@ public:
                 (::google::protobuf::RpcController * controller,
                  const ::firebolt::rialto::GetImmediateOutputRequest *request,
                  ::firebolt::rialto::GetImmediateOutputResponse *response, ::google::protobuf::Closure *done));
-    MOCK_METHOD(void, setReportDecodeErrors,
-                (::google::protobuf::RpcController * controller,
-                 const ::firebolt::rialto::SetReportDecodeErrorsRequest *request,
-                 ::firebolt::rialto::SetReportDecodeErrorsResponse *response, ::google::protobuf::Closure *done));
-    MOCK_METHOD(void, getQueuedFrames,
-                (::google::protobuf::RpcController * controller, const ::firebolt::rialto::GetQueuedFramesRequest *request,
-                 ::firebolt::rialto::GetQueuedFramesResponse *response, ::google::protobuf::Closure *done));
     MOCK_METHOD(void, getStats,
                 (::google::protobuf::RpcController * controller, const ::firebolt::rialto::GetStatsRequest *request,
                  ::firebolt::rialto::GetStatsResponse *response, ::google::protobuf::Closure *done));
@@ -235,19 +228,6 @@ public:
     {
         firebolt::rialto::GetImmediateOutputResponse response;
         response.set_immediate_output(immediateOutputResponse);
-        return response;
-    }
-
-    ::firebolt::rialto::SetReportDecodeErrorsResponse SetReportDecodeErrorsResponse()
-    {
-        firebolt::rialto::SetReportDecodeErrorsResponse response;
-        return response;
-    }
-
-    ::firebolt::rialto::GetQueuedFramesResponse getQueuedFramesResponse(uint32_t queuedFramesResponse)
-    {
-        firebolt::rialto::GetQueuedFramesResponse response;
-        response.set_queued_frames(queuedFramesResponse);
         return response;
     }
 
