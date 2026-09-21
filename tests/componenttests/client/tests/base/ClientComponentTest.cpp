4713c81c08f6f06bf128bc3f48837afbff73287e
uganes412_comcast <Usha_Ganeshpandian@comcast.com>
2026-09-21 17:12:01 +0530

    RDKEMW-23564: Fix on clagg and cpplint

--- tests/componenttests/client/tests/base/ClientComponentTest.cpp
+++ tests/componenttests/client/tests/base/ClientComponentTest.cpp
@@ -114,7 +114,7 @@ ClientComponentTest::ClientComponentTest()
       WebAudioPlayerTestMethods(getWebAudioPartitions()),
       m_privateMetricsModuleMock{std::make_shared<NiceMock<PrivateMetricsModuleMock>>()},
       m_serverStub{std::make_shared<ServerStub>(m_controlModuleMock, m_mediaPipelineModuleMock,
-	  											m_mediaCapabilitiesModuleMock, m_mediaKeysModuleMock,
+                                                m_mediaCapabilitiesModuleMock, m_mediaKeysModuleMock,
                                                 m_mediaKeysCapabilitiesModuleMock, m_mediaPipelineCapabilitiesModuleMock,
                                                 m_webAudioPlayerModuleMock, m_privateMetricsModuleMock)}
 {
